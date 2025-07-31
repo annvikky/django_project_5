2c4c52b6019fd253d4c378eed785b648e47794d5
Анна Кочерыгина <annvikky@gmail.com>
2025-07-31 11:04:23 +0800

    test ci

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from datetime import timedelta
 from pathlib import Path
 
@@ -178,3 +179,11 @@ CELERY_TIMEZONE = TIME_ZONE
 CELERY_TASK_TRACK_STARTED = True
 
 CELERY_TASK_TIME_LIMIT = 30 * 60
+
+if "test" in sys.argv:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "test_db_sqlite3",
+        }
+    }
